209e4e6fc7abfa459b89a7608bb8528efc2b111d
rahuljha661 <rahuljha12527@gmail.com>
2020-10-17 11:55:46 +0530

    complete the ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CheckBox from './components/CheckBox';
 
-import './App.css';
+
 
 function App() {
   return (
--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -7,37 +7,56 @@ import React, { Component } from 'react'
                 <form className="formData">
                     <div>
                      <h3>Portugal</h3>
+                     <div>
                      <label>
                     <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
                     Aasiya Jayant</label>
+                    </div>
+                    <div>
                     <label>
                     <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
                     Luvieen Lawrence</label>
+                    </div>
+                    <div>
                     <label>
+                    
                     <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
                     Rey Miboume</label>
                     </div>
                     <div>
                     <h3>Nicaragau</h3>
+                    </div>
+                    <div>
                     <label>
+                        
                     <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
                     Deveedaas Nandi</label>
+                    </div>
+                    <div>
                     <label>
                     <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
                     Obasey Chidy</label>
+                    </div>
+                    <div>
                     <label>
                     <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
                     Xenie Dolezelova</label>
                     </div>
+                    </div>
                     <div>
+                        
                     <h3>Marshall Islands</h3>
+                    <div>
                     <label>
                     <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
                     Aasiya Jayant</label>
+                    </div>
+                    <div>
                     <label>
+                   
                     <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
                     Jelina Devisova</label>
-                  
+                    </div>
                     </div>
 
                 </form>
