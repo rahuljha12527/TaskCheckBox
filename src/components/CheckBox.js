599922c65706917b9c3f8e5912eb6c4396ba92f6
rahuljha661 <rahuljha12527@gmail.com>
2020-10-17 11:50:28 +0530

    add checkbox

---
+++ src/components/CheckBox.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+ class CheckBox extends Component {
+    render() {
+        return (
+            <div className="backC">
+                <form className="formData">
+                    <div>
+                     <h3>Portugal</h3>
+                     <label>
+                    <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
+                    Aasiya Jayant</label>
+                    <label>
+                    <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
+                    Luvieen Lawrence</label>
+                    <label>
+                    <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
+                    Rey Miboume</label>
+                    </div>
+                    <div>
+                    <h3>Nicaragau</h3>
+                    <label>
+                    <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
+                    Deveedaas Nandi</label>
+                    <label>
+                    <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
+                    Obasey Chidy</label>
+                    <label>
+                    <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
+                    Xenie Dolezelova</label>
+                    </div>
+                    <div>
+                    <h3>Marshall Islands</h3>
+                    <label>
+                    <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
+                    Aasiya Jayant</label>
+                    <label>
+                    <input type="checkbox" name="Aasiya Jayant" value="Aasiya Jayant" ></input>
+                    Jelina Devisova</label>
+                  
+                    </div>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default CheckBox;
